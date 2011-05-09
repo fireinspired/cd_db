a5c48588df9cfee4e970303051053a5a40856983
Oliver Booth <oliver.george.booth.07@aberdeen.ac.uk>
2011-05-09 21:34:26 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CdDb::Application.config.secret_token = '50fe6e92a35577423036bd74479d24c53bc1de373fcf912426340e8d1abc398917d0b6a016383224692974aefac1c60e45c8dde256c7c70721d094adb5fad6dc'
